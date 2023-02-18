f855685492ba6202d4a95e1f35032e636fb0d3fe
leksyking <foogundipe@student.oauife.edu.ng>
2023-02-18 20:13:30 +0100

    add remix solidity basics codes

---
+++ remix-solidity-basics-code/SafeMathTester.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SafeMathTester{
+    uint8 public bigNumber = 220;
+
+    function add() public {
+        bigNumber++;
+    }
+}
