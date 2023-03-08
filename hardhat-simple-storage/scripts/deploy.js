2a904bb214b4fe765383e3621c1d9085a67d509c
leksyking <foogundipe@student.oauife.edu.ng>
2023-03-08 21:28:30 +0100

    start working on hardhat version of simple storage contract

---
+++ hardhat-simple-storage/scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const SimpleStorage = await SimpleStorageFactory.deploy()
+    await SimpleStorage.deployed()
+
+    console.log(`Deployed contract to: ${SimpleStorage.address}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
