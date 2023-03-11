a015f785e81c444dabbf69b92ed17d549b73e9c8
leksyking <foogundipe@student.oauife.edu.ng>
2023-03-11 14:12:50 +0100

    verify the contract

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
-//use goerli
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
@@ -15,6 +16,9 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 5,
         },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
     solidity: "0.8.9",
 }
