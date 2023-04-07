c93a9ff1fd7728f52068fd5e69f2ce8f314b4a6a
leksyking <foogundipe@student.oauife.edu.ng>
2023-04-07 19:48:22 +0100

    deploy both the mock contract and the fundMe

--- hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -1,5 +1,5 @@
 const { network } = require("hardhat")
-const { networkConfig } = require("../helper-hardhat-config")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     console.log("Hiii!!!")
@@ -7,7 +7,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    const ethUsdPriceFeedAddress = networkConfig[chainId][ethUsdPriceFeed]
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
 
     // what happenss when we want to change chains
     // when going for localhost or hardhat network we want to use a mock
@@ -15,6 +22,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const fundMe = await deploy("FundMe", {
         from: deployer,
         args: [ethUsdPriceFeedAddress], //put price feed address
-        logs: true,
+        log: true,
     })
+
+    log("----------------------------------------------------")
 }
+
+module.exports.tags = ["all", "fundMe"]
--- hardhat-fund-me/helper-hardhat-config.js
+++ hardhat-fund-me/helper-hardhat-config.js
@@ -10,6 +10,14 @@ const networkConfig = {
     //31337
 }
 
+const developmentChains = ["hardhat", "localhost"]
+
+const DECIMALS = "8"
+const INITIAL_ANSWER = "200000000000"
+
 module.exports = {
     networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
 }
